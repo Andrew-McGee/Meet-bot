4f1b904707df23c7357c8a2063ca59c71421c8a9
Andrew McGee <45531427+Andrew-McGee@users.noreply.github.com>
2021-11-04 12:59:13 +1100

    Update chat-bot.js

--- chat-bot.js
+++ chat-bot.js
@@ -1,20 +1,20 @@
 // #####################################################
-// ##            Google Meet chat bot	                ##
+// ##            Google Meet chat bot                 ##
 // ##  Original fork from Useems (c) 2021 MIT License ##
 // ##  https://github.com/Useems/google-meet-chatbot  ##
-// ##              				                            ##
-// ##  This version 2021 (c) MIT License	        	  ##
-// ##  https://github.com/Andrew-McGee            		##
+// ##                                                 ##
+// ##  This version 2021 (c) MIT License              ##
+// ##  https://github.com/Andrew-McGee                ##
 // ##                                                 ##
 // ## This is a HACK on the DOM - there is no public  ##
-// ## API for Google Meet chat.           				    ##
+// ## API for Google Meet chat.                       ##
 // ##                                                 ##
-// ## To run open DevTools in browser.               	##
+// ## To run open DevTools in browser.                ##
 // ## Ensure Google Meet chat window is open.         ##
-// ## Paste script into the console.             			##
-// ## Hit enter to execute.             				      ##
+// ## Paste script into the console.                  ##
+// ## Hit enter to execute.                           ##
 // ## Standby with fire extinguisher.                 ##
-// ##              				                            ##
+// ##                                                 ##
 // #####################################################
 (() => {
 	let messagesLength = -1;
